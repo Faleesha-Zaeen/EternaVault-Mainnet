a599e6bb81ef08eacec7ad838cf5735ab53a1653
Faleesha Zaeen Zarshad <faleeshazaeenzarshad.it2024@citchennai.net>
2025-12-11 21:23:27 +0530

    Fix storage.js to use NFT.Storage

--- frontend/src/utils/storage.js
+++ frontend/src/utils/storage.js
@@ -1,11 +1,27 @@
-import { NFTStorage, File } from 'nft.storage';
+// import { NFTStorage, File } from 'nft.storage';
+
+// export async function uploadEncryptedBlob(blob, token) {
+//   if (!token) {
+//     throw new Error('Missing NFT.Storage token');
+//   }
+
+//   const client = new NFTStorage({ token });
+//   const cid = await client.storeBlob(new File([blob], 'encrypted.enc'));
+//   return cid;
+// }
+import { NFTStorage } from 'nft.storage'
 
 export async function uploadEncryptedBlob(blob, token) {
-  if (!token) {
-    throw new Error('Missing NFT.Storage token');
-  }
+  if (!token) throw new Error("Missing NFT.Storage token")
 
-  const client = new NFTStorage({ token });
-  const cid = await client.storeBlob(new File([blob], 'encrypted.enc'));
-  return cid;
+  const client = new NFTStorage({ token })
+
+  // Convert encrypted blob to File for upload
+  const file = new File([blob], "encrypted.enc", {
+    type: "application/octet-stream",
+  })
+
+  // Upload to IPFS via NFT.Storage
+  const cid = await client.storeBlob(file)
+  return cid
 }
